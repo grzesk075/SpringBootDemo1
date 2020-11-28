1b0b8c2ffc621ac9470e7cc26bbaa5e89c14aac1
Grzegorz Kuprianowicz <grzesk075@gmail.com>
2020-11-28 12:52:37 +0100

    Hypermedia-based RESTful front end and a JPA-based back end.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # SpringBootDemo1
 Spring Boot sandbox
+
+To build `mvn clean install`, or even better by Maven Wrapper `./mvnw clean install`.
+
+To run just start on IntelliJ Idea main class with `@SpringBootApplication` (`DemoApplication`),
+or type `./mvnw spring-boot:run`.
+
+Prerequisites:
+* run mysql on docker using scripts from `./mysql` folder.
+
+Main page: http://localhost:8080/index0.html.
+
+Top level RESTful services are listed automatically: http://localhost:8080.
---
+++ src/main/java/com/example/accessingdatarest/PersonRepository.java
@@ -0,0 +1,29 @@
+package com.example.accessingdatarest;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+/**
+ * Hypermedia-based RESTful front end and a JPA-based back end.
+ * At runtime, Spring Data REST automatically creates an implementation of this interface.
+ * Then it uses the @RepositoryRestResource annotation to direct Spring MVC to create RESTful endpoints at /people.
+ * @RepositoryRestResource is not required for a repository to be exported.
+ * It is used only to change the export details, such as using /people instead of the default value of /persons.
+ *
+ * http://localhost:8080/people   contains content-type: application/hal+json
+ * HAL - Hypertext Application Language - enables discoverability within API
+ *
+ * add (POST) curl -i -H "Content-Type:application/json" -d '{"firstName": "Frodo", "lastName": "Baggins"}' http://localhost:8080/people
+ * find (GET): curl http://localhost:8080/people
+ * custom find (GET): curl http://localhost:8080/people/search/findByLastName?name=Baggins
+ * replaces an entire record: curl -X PUT -H "Content-Type:application/json" -d '{"firstName": "Bilbo", "lastName": "Baggins"}' http://localhost:8080/people/1
+ * update subset of fields: curl -X PATCH -H "Content-Type:application/json" -d '{"firstName": "Bilbo Jr."}' http://localhost:8080/people/1
+ * remove record: curl -X DELETE http://localhost:8080/people/1
+ */
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+    List<Person> findByLastName(@Param("name") String name);
+}
